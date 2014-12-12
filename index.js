a6390b57851d954f1b05a798dea3fa521a7efef1
Cody Schwebke <cschwebk@yahoo-inc.com>
2014-12-11 16:24:43 -0800

    address comments

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var Bundler = require('./lib/bundler');
+var Bundler = require('./lib/bundler'),
+    browserify = require('browserify');
 
 var bundler = module.exports = function(config) {
 
     return new Bundler(config);
 };
+
+// expose browserify lib
+bundler.browserify = browserify;
